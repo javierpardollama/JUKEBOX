6e1ce6989ea3a543a3d8348055654e542b7e75da
javierpardollama <javierpardollama@outlook.com>
2023-11-24 09:29:12 +0100

    Control de Nulos & Variable Binaria

--- ia/linear.py
+++ ia/linear.py
@@ -6,16 +6,15 @@ from sklearn.model_selection import train_test_split
 
 df = pandas.read_json("../data/data.json")
 
-d = {'Rock': 0, 'Pop': 1, 'Pop Rock': 2, 'Alternative Rock':3, 'Flamenco':4, 'Electronic':5}
-df['gender'] = df['gender'].map(d)
+df = df.dropna()
 
-df = df[df['year'].notnull()]
-df = df[df['year'].notna()]
+d = {False: 0, True: 1}
+df['bought'] = df['bought'].map(d)
 
-features = ['year']
+features = ['likes']
 
 x = df[features]
-y = df['gender']
+y = df['bought']
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 
@@ -33,7 +32,7 @@ print("Coefficient of determination: %.2f" % r2_score(y_test, z_pred))
 plt.scatter(x_test, y_test, color="black")
 plt.plot(x_test, z_pred, color="blue", linewidth=3)
 
-plt.xlabel('year')
-plt.ylabel('gender')
+plt.xlabel('likes')
+plt.ylabel('bought')
 
 plt.show()
--- ia/logistic.py
+++ ia/logistic.py
@@ -6,15 +6,17 @@ from sklearn.model_selection import train_test_split
 
 df = pandas.read_json("../data/data.json")
 
-d = {'Rock': 0, 'Pop': 1, 'Pop Rock': 2, 'Alternative Rock':3, 'Flamenco':4, 'Electronic':5}
-df['gender'] = df['gender'].map(d)
+df = df.dropna()
 
-features = ['year']
+d = { False: 0, True: 1}
+df['bought'] = df['bought'].map(d)
+
+features = ['likes']
 
 x = df[features]
-y = df['gender']
+y = df['bought']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=50)
 
 logistic = linear_model.LogisticRegression()
 logistic = logistic.fit(x_train, y_train)
@@ -25,9 +27,10 @@ score = logistic.score(x_test, y_test)
 print("Acuracy: \n",score)
 
 plt.scatter(x_test, y_test, color="black")
-plt.plot(x_test, z_pred, color="blue", linewidth=3)
 
-plt.xlabel('year')
-plt.ylabel('gender')
+plt.plot(x_test, z_pred, color='red')
+
+plt.xlabel('likes')
+plt.ylabel('bought')
 
 plt.show()
